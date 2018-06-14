96094ea29c8294bba93c6d1103d5f9321b7b689c
masi92 <39678756+masi92@users.noreply.github.com>
2018-06-14 17:33:39 +0300

    napit + js

---
+++ js/js.js
@@ -0,0 +1,19 @@
+document.getElementById("cubes").onmouseover = function() {mouseOver()};
+function mouseOver() {
+    document.getElementById("cubes").style.color = "red";
+}
+
+document.getElementById("gears").onmouseover = function() {mouseOver()};
+function mouseOver() {
+    document.getElementById("gears").style.color = "red";
+}
+
+document.getElementById("heartbeat").onmouseover = function() {mouseOver()};
+function mouseOver() {
+    document.getElementById("heartbeat").style.color = "red";
+}
+
+
+$('#esittelyCarousel').onmouseover('slide.bs.carousel', function () {
+  // do something…
+})
