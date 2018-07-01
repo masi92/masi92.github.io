85a92bc8b77189794f6f009b8caa6df2b985e432
masi92 <39678756+masi92@users.noreply.github.com>
2018-07-01 17:42:51 +0300

    Arvostelut + small fixes

--- js/js.js
+++ js/js.js
@@ -1,19 +1,34 @@
-document.getElementById("cubes").onmouseover = function() {mouseOver()};
-function mouseOver() {
-    document.getElementById("cubes").style.color = "red";
-}
 
-document.getElementById("gears").onmouseover = function() {mouseOver()};
-function mouseOver() {
-    document.getElementById("gears").style.color = "red";
-}
 
 document.getElementById("heartbeat").onmouseover = function() {mouseOver()};
 function mouseOver() {
     document.getElementById("heartbeat").style.color = "red";
 }
 
+document.getElementById("heartbeat").onmouseout = function() {mouseOut()};
+function mouseOut() {
+    document.getElementById("heartbeat").style.color = "black";
+}
 
-$('#esittelyCarousel').onmouseover('slide.bs.carousel', function () {
-  // do something…
+// $('#esittelyCarousel').onmouseover('slide.bs.carousel', function () {
+//     do something…
+// })
+
+
+$('#esittelyCarousel').carousel({
+   interval: false
+});
+
+var i;
+
+$('.carousel-control').on("mouseover", function () {
+   var control = $(this),
+       interval = 500;
+
+   i = setInterval(function () {
+       control.trigger("click");
+   }, interval);
 })
+.on("mouseout", function () {
+   clearInterval(i);
+});
